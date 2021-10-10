566659f3d8b636c75962629d95aea6503c3292c9
helenalvp <helenalvp@gmail.com>
2021-10-10 19:15:44 -0400

    Add project files.

---
+++ TacoCatMVC/wwwroot/js/site.js
@@ -0,0 +1,31 @@
+﻿
+function getValue() {
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    if (userString !== "") {
+        let revString = reverseString(userString);
+        displayString(revString);
+    } else {
+        alert("Please enter a string")
+        5
+    }
+}
+
+function reverseString(userStr) {
+    0000code
+
+
+    for (let i = userStr.length - 1; i >= 0; i--) {
+        revStr += userStr[i];
+    }
+
+    return revStr;
+}
+
+function displayString(reversedStr) {
+
+    document.getElementById("msg").innerHTML = `Your string backwards is: ${reversedStr}`
+    document.getElementById("alert").classList.remove("invisible");
+}
